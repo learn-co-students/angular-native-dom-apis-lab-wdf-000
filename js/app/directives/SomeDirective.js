6468562bd647fb41991d408e1c92727e32a8e5d6
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-27 14:41:00 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -1,21 +1,30 @@
 function SomeDirective() {
-	return {
-		template: [
-			'<div>',
-				'<h3>{{ some.text }}</h3>',
-				'<span>Click here to replace the text</span>',
-			'</div>'
-		].join(''),
-		controller: function() {
-			this.text = 'Replace this text!';
-		},
-		controllerAs: 'some',
-		link: function (scope, elem, attrs) {
+  return {
+    template: [
+      '<div>',
+      '<h3>{{ some.text }}</h3>',
+      '<span>Click here to replace the text</span>',
+      '</div>'
+    ].join(''),
+    controller: function() {
+      this.text = 'Replace this text!';
+    },
+    controllerAs: 'some',
+    require: 'someDirective',
+    link: function (scope, elem, attrs, ctrl) {
+      ctrl.text = 'Thank you for clicking!';
+      var actualElem = elem[0];
+      var spanElem = actualElem.querySelector('span');
 
-		}
-	}
+      // spanElem.addEventListener('click', function() {
+      actualElem.addEventListener('click', function() {
+	spanElem.innerHTML(ctrl.text);
+	$scope.apply();
+      });
+    }
+  }
 }
 
 angular
-	.module('app')
-	.directive('someDirective', SomeDirective);
+  .module('app')
+  .directive('someDirective', SomeDirective);
